6723bcb8ed47357d1428b9534f2f96136c404a6a
Rajeshrajesh2005 <145636711+Rajeshrajesh2005@users.noreply.github.com>
2023-09-21 09:46:15 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,16 @@
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+print (factorial(5))
+def factoial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+print (factorial(5))
+
+ 
+  
+
